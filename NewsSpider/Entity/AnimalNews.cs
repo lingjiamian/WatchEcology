bc20492133893c35da88b4fc6b32920bb03a53b9
JameLing <lingjiamian@outlook.com>
2020-12-29 02:18:09 +0800

    上传到github

---
+++ NewsSpider/Entity/AnimalNews.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsSpider.Entity
+{
+    public class AnimalNews
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string ImgUrl { get; set; }
+        public string Source { get; set; }
+        public DateTime? PubDate { get; set; }
+    }
+}
